f7ad3cd972138794e5547e5bd4cafa37521f5a18
Nick Berkshire-Lilley <nick18bl@gmail.com>
2026-04-29 15:59:10 +0100

    put css + js into includes RMS

--- RMS/public/Options/RMS-Mockup-StudentRec.html.php
+++ RMS/public/Options/RMS-Mockup-StudentRec.html.php
@@ -1,17 +1,17 @@
 <?php
-require "../../Includes/RMS-Mockup-HEADER.html";
+require "../Includes/RMS-Mockup-HEADER.html";
 ?>
 <div id="list">
     <ul>
-        <li class="item"><a id="hideLink" href="../RMS-Mockup-StudentRec.html.php" >Overview</a></li>
-        <li class="item"><a id="hideLink" href="../RMS/Options/StudentRec/RMS-Mockup-StuApplication.html.php" >Student Applications</a></li>
-        <li class="item"><a id="hideLink" href="../RMS/Options/StudentRec/RMS-Mockup-StuCurrent.html.php" >Current Students</a></li>
-        <li class="item" id="thatOne"><a id="hideLink" href="../RMS/Options/StudentRec/RMS-Mockup-StuPast.html.php" >Past Students</a></li>
+        <li class="item"><a id="hideLink" href="../Options/RMS-Mockup-StudentRec.html.php" >Overview</a></li>
+        <li class="item"><a id="hideLink" href="../Options/StudentRec/StudentRec/RMS-Mockup-StuApplication.html.php" >Student Applications</a></li>
+        <li class="item"><a id="hideLink" href="../Options/StudentRec/RMS-Mockup-StuCurrent.html.php" >Current Students</a></li>
+        <li class="item" id="thatOne"><a id="hideLink" href="../Options/StudentRec/RMS-Mockup-StuPast.html.php" >Past Students</a></li>
     </ul>
 </div>
 
 <h1>Student Records</h1>
 
 <?php
-require "../../Includes/RMS-Mockup-FOOTER.html";
+require "../Includes/RMS-Mockup-FOOTER.html";
 ?>
--- RMS/public/Options/StudentRec/RMS-Mockup-StuApplication.html.php
+++ RMS/public/Options/StudentRec/RMS-Mockup-StuApplication.html.php
@@ -1,15 +1,14 @@
 <?php
-require "../../Includes/RMS-Mockup-HEADER.html";
+require "/Includes/../RMS-Mockup-HEADER.HTML";
 ?>
 <div id="list">
     <ul>
-        <li class="item"><a id="hideLink" href="../RMS-Mockup-StudentRec.html.php" >Overview</a></li>
-        <li class="item"><a id="hideLink" href="../RMS/Options/StudentRec/RMS-Mockup-StuApplication.html.php" >Student Applications</a></li>
-        <li class="item"><a id="hideLink" href="../RMS/Options/StudentRec/RMS-Mockup-StuCurrent.html.php" >Current Students</a></li>
-        <li class="item" id="thatOne"><a id="hideLink" href="../RMS/Options/StudentRec/RMS-Mockup-StuPast.html.php" >Past Students</a></li>
+        <li class="item"><a id="hideLink" href="../RMS-Mockup-StaffRec.html.php" >Overview</a></li>
+        <li class="item"><a id="hideLink" href="../studentRec/RMS-Mockup-StuApplication.html.php" >Student Applications</a></li>
+        <li class="item"><a id="hideLink" href="../studentRec/RMS-Mockup-StuCurrent.html.php" >Current Students</a></li>
+        <li class="item" id="thatOne"><a id="hideLink" href="../studentRec/RMS-Mockup-StuPast.html.php" >Past Students</a></li>
     </ul>
 </div>
-
 <h1>Student Applications</h1>
 
 <!--LIST OF STAFF! -->
