631cbdc897a94260fbd6fc7ba76a83a99e65bd6a
Adam Robson <adamrobson06@outlook.com>
2026-04-30 10:58:35 +0100

    Added controller templates for Marcus and Divine to expand on

---
+++ RMS/controllers/controller.php
@@ -0,0 +1,68 @@
+<?php
+namespace WUC;
+
+class controllerRMS {
+
+    private $assignmentsTable;
+    private $attendanceTable;
+    private $chatlogsTable;
+    private $courseModulesLinkTable;
+    private $coursesTable;
+    private $emergencyContactsTable;
+    private $moduleAssignmentsTable;
+    private $modulesTable;
+    private $personalTutorialsTable;
+    private $recordStatusesTable;
+    private $staffTable
+    private $studentAssignmentsTable;
+    private $studentsTable;
+    private $ticketsTable;
+    private $timetable
+
+
+    // Main Constructor
+
+    public function __construct($assignmentsTable, $attendanceTable, $chatlogsTable, $coursesModulesLinkTable, $coursesTable, $emergencyContactsTable, $moduleAssignmentsTable, $modulesTable, $personalTutorialsTable, $recordStatusesTable, $staffTable, $studentAssignmentsTable, $studentsTable, $ticketsTable, $timetable) {
+
+        $this->assignmentsTable = $assignmentsTable;
+        $this->attendanceTable = $attendanceTable;
+        $this->chatlogsTable = $chatlogsTable;
+        $this->courseModulesLinkTable = $courseModulesLinkTable;
+        $this->coursesTable = $coursesTable;
+        $this->emergencyContactsTable = $emergencyContactsTable;
+        $this->moduleAssignmentsTable = $moduleAssignmentsTable;
+        $this->modulesTable = $modulesTable;
+        $this->personalTutorialsTable = $personalTutorialsTable;
+        $this->recordStatusesTable = $recordStatusesTable;
+        $this->staffTable = $staffTable;
+        $this->studentAssignmentsTable = $studentAssignmentsTable;
+        $this->studentsTable = $studentsTable;
+        $this->ticketsTable = $ticketsTable;
+        $this->timetable = $timetable;
+
+
+    }
+
+}
+
+/*
+
+    An example of the loadTemplate function in action
+
+        echo loadTemplate('../templates/layout.html.php', [
+        'title' => 'UON Home',
+        'linkOutput' => $linkOutput,
+        'subjectSearch' => '',
+        'output' => loadTemplate('../templates/' . ltrim(explode('?', $_SERVER['REQUEST_URI'])[0], '/') . '.html.php', [
+
+            'loginLinkOutput' => $loginLinkOutput,
+            'managementOutput' => $managementOutput
+
+        ])]);
+
+    Step 1: Pass the file location of the template in as the first argument in single quotes
+    Step 2: Pass in variables that the template will need to load
+    Step 3: Load subtemplates via the same method
+
+    
+*/
