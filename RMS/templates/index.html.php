6acf837c7e8557c0219d68b0555a4e188212b8ec
Nick Berkshire-Lilley <nick18bl@gmail.com>
2026-05-11 17:06:00 +0100

    test

--- RMS/templates/index.html.php
+++ RMS/templates/index.html.php
@@ -10,17 +10,41 @@ require "../templates/RMS-Mockup-HEADER.html";
 <hr id="gap">
 
 <div class="MainContent">
-    <img src="https://picsum.photos/536/364" alt="">
-    <h1 class="MainContentH">Welcome to the RMS</h1>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum officiis laboriosam similique doloribus laborum nisi recusandae ut earum fuga unde natus repellendus deleniti, animi impedit, et voluptatibus minima omnis aperiam.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam molestias exercitationem voluptate natus veritatis accusantium earum voluptas repellendus, officiis praesentium fugit eum eligendi deserunt rem possimus sed eaque recusandae temporibus.
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod dolorem ad voluptatum nesciunt. Quos est autem eveniet recusandae harum fugiat expedita magni totam, eum ipsa natus, minima modi dicta deleniti!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quam nemo itaque voluptates debitis totam voluptatum, odit corporis cum eaque hic natus sit ipsam repellat ea neque? Accusamus, obcaecati tenetur.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima a, blanditiis iure, eaque illo itaque autem ullam deleniti laborum quo voluptates perferendis ab sint quasi velit facilis error suscipit modi.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta numquam accusamus nostrum cum, dolores assumenda aut quae amet ad maiores facilis sunt, magni eligendi recusandae quis quod pariatur aliquam. Harum.
-</p>
-</div>
+ <div class="slideshow-container">
 
+<!--https://www.w3schools.com/howto/howto_js_slideshow.asp-->
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img class="imgSize" src="https://www.northampton.ac.uk/wp-content/uploads/2024/09/waterside-campus-external-and-river-gallery.jpg">
+    <div class="infoCard">Caption One</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img class="imgSize" src="https://www.northampton.ac.uk/wp-content/uploads/2022/08/learning-hub-stairs-gallery.jpg" style="width:100%">
+    <div class="infoCard">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img class="imgSize" src="https://www.northampton.ac.uk/wp-content/uploads/2022/08/learning-hub-screen-gallery.jpg" style="width:100%">
+    <div class="infoCard">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
 
 <!--To follow what I had proposed in the wireframe, I want to add an image carousel here -->
 <?php
